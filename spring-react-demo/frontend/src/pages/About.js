f93a005e06f28bf34318578001a65b7e32f33194
moong <who7117@outlook.kr>
2020-01-10 11:49:29 +0900

    react routes

---
+++ spring-react-demo/frontend/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CssBaseline } from '@material-ui/core';
+
+function About() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div>
+        <h1>about</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default About;
---
+++ spring-react-demo/frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CssBaseline } from '@material-ui/core';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div>
+        <h1>home</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
